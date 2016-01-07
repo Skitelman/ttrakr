add9cdd2a7b5d45c926b82325aa168eb5d4792b6
Skitelman <sghitelman@gmail.com>
2016-01-07 10:57:54 -0500

    added migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   root "home#index"
   get "random_shirt", to: "random_shirt#random"
+  # get "shirts", to: "shirts#index"
+  # get "shirts/:id", to: "shirts#show"
+  resources :shirts, only: [:index, :show]
 end
+
+=begin
+get shirts => all
+get shirts/7 => One shirt
+get shirts/new => A form to create a new shirt
+get shirts/7/edit => all
+=end
