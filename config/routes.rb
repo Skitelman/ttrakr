f0ecb43a5b829b993e1b54c897a673d07d06b7df
Skitelman <sghitelman@gmail.com>
2016-01-12 11:59:36 -0500

    added comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   root "home#index"
   get "random_shirt", to: "random_shirt#random"
   resources :shirts, only: [:index, :show]
-  resources :lessons, only: [:index, :show, :new, :create]
+  resources :lessons, only: [:index, :show, :new, :create] do
+    resources :comments, only: [:create]
+  end
   get 'sign-in', to: "sessions#new"
   post 'sign-in', to: "sessions#create"
   delete 'sign-out', to: "sessions#destroy"
 
   get 'sign-up', to: "registrations#new"
   post 'sign-up', to: "registrations#create"
+
 end
